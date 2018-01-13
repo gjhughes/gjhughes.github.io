5cc3a4a7af007a3ddd3a1131a4872b4eb8fd0b57
gjhughes <ghughes@me.com>
2018-01-13 11:56:21 +0000

    links

--- js/main.js
+++ js/main.js
@@ -4,50 +4,69 @@ particlesJS.load('particles-js', 'particles.json', function() {
 });
 
 $(document).ready(function() {
-
-  // Scroll to skills 
-  $(".foot-wrapper").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".skills-heading").offset().top},
-        1000);
+  // Scroll to skills
+  $('.foot-wrapper').click(function() {
+    $('html,body').animate(
+      {
+        scrollTop: $('.skills-heading').offset().top,
+      },
+      1000
+    );
   });
 
-  // Scroll to projects 
-  $(".skills-foot-wrapper").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".projects-wrapper").offset().top},
-        1000);
+  // Scroll to projects
+  $('.skills-foot-wrapper').click(function() {
+    $('html,body').animate(
+      {
+        scrollTop: $('.projects-wrapper').offset().top,
+      },
+      1000
+    );
   });
 
   // Scroll to contact
-  $(".projects-foot-wrapper").click(function() {
-    $('html,body').animate({
-      scrollTop: $(".contact-wrapper").offset().top},
-      1000);
+  $('.projects-foot-wrapper').click(function() {
+    $('html,body').animate(
+      {
+        scrollTop: $('.contact-wrapper').offset().top,
+      },
+      1000
+    );
   });
 
-  $('.project-one').hover(function() {
-    $('.hover-content-one').fadeIn();
-  }, function() {
-    $('.hover-content-one').fadeOut();
-  });
-  
-  $('.project-two').hover(function() {
-    $('.hover-content-two').fadeIn();
-  }, function() {
-    $('.hover-content-two').fadeOut();
-  });
+  $('.project-one').hover(
+    function() {
+      $('.hover-content-one').fadeIn();
+    },
+    function() {
+      $('.hover-content-one').fadeOut();
+    }
+  );
 
-  $('.project-three').hover(function() {
-    $('.hover-content-three').fadeIn();
-  }, function() {
-    $('.hover-content-three').fadeOut();
-  });
+  $('.project-two').hover(
+    function() {
+      $('.hover-content-two').fadeIn();
+    },
+    function() {
+      $('.hover-content-two').fadeOut();
+    }
+  );
 
-  $('.project-four').hover(function() {
-    $('.hover-content-four').fadeIn();
-  }, function() {
-    $('.hover-content-four').fadeOut();
-  });
+  $('.project-three').hover(
+    function() {
+      $('.hover-content-three').fadeIn();
+    },
+    function() {
+      $('.hover-content-three').fadeOut();
+    }
+  );
 
+  $('.project-four').hover(
+    function() {
+      $('.hover-content-four').fadeIn();
+    },
+    function() {
+      $('.hover-content-four').fadeOut();
+    }
+  );
 });
